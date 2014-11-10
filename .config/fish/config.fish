b5b178d4f0ca9cb96f1c3a3a297afe9f463ae4eb
Jason Bellamy <j@sonbellamy.com>
2014-11-10 16:37:33 -0500

    fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,28 @@
 set -x EDITOR vim
+
+# Navigation
+function ..    ; cd .. ; end
+function ...   ; cd ../.. ; end
+
+# Utilities
+function g; hub $argv ; end
+
+
+function fish_prompt
+  git_branch
+
+  set_color normal
+  printf '%s ' (prompt_pwd)
+
+  set_color white
+  printf '[%s' $git_branch
+  if test $git_dirty_count -gt 0
+    printf ':'
+    set_color red
+    printf $git_dirty_count
+    set_color white
+  end
+  printf ']'
+  set_color normal
+  printf ' » '
+end
